ca4bb6cbcec50667da8de6ab6b543390b5d559d9
Erin Timlin <erintimlin@gmail.com>
2023-02-23 18:40:52 -0500

    add initial files

---
+++ ProjectName.Tests/ModelTest/ClassNameTests.cs
@@ -0,0 +1,12 @@
+ProjectName.Solution/ProjectName.Tests/ModelTests/ClassNameTests.cs
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName.Models;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class ClassNameTests
+  {
+    // Test methods go here
+  }
+}
